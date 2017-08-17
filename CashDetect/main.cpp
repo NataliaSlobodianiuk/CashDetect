e49fcd5e4c6167f03205f74ff146ac1ca7b0b9d5
NataliaSlobodianiuk <slo0nata@gmail.com>
2017-08-17 20:15:12 +0300

    threshold improved

--- CashDetect/main.cpp
+++ CashDetect/main.cpp
@@ -19,19 +19,21 @@ int main(int argc, char **argv)
 	Mat src_gray;
 	cvtColor(src, src_gray, COLOR_BGR2GRAY);
 
-	GaussianBlur(src_gray, src_gray, Size(9, 9), 2, 2);
+	GaussianBlur(src_gray, src_gray, Size(3, 3), 0, 0);
 
+	double min_radius = src.cols / 50;
 	vector<Vec3f> circles;
+	HoughCircles(src_gray, circles, HOUGH_GRADIENT, 1, min_radius * 2, 100, 65, min_radius);
 
-	HoughCircles(src_gray, circles, HOUGH_GRADIENT, 1, src_gray.rows / 8, 200, 100, 0, 0);
+	cout << circles.size() << endl;
 
 	for (size_t i = 0; i < circles.size(); i++)
 	{
 		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
 		int radius = cvRound(circles[i][2]);
 
-		circle(src, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-		circle(src, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+		circle(src, center, 1, Scalar(0, 255, 0), -1, 8, 0);
+		circle(src, center, radius, Scalar(0, 0, 255), 1, 8, 0);
 	}
 
 	imshow("Result", src);
