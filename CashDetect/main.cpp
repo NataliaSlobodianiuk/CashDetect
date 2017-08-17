c5cdc4fb7b3a8d7727f027acc70ae33d3d5778aa
NataliaSlobodianiuk <slo0nata@gmail.com>
2017-08-17 13:03:40 +0300

    initiate

--- CashDetect/main.cpp
+++ CashDetect/main.cpp
@@ -16,52 +16,25 @@ int main(int argc, char **argv)
 
 	imshow("Source Image", src);
 
-	for (int x = 0; x < src.rows; x++) 
+	Mat src_gray;
+	cvtColor(src, src_gray, COLOR_BGR2GRAY);
+
+	GaussianBlur(src_gray, src_gray, Size(9, 9), 2, 2);
+
+	vector<Vec3f> circles;
+
+	HoughCircles(src_gray, circles, HOUGH_GRADIENT, 1, src_gray.rows / 8, 200, 100, 0, 0);
+
+	for (size_t i = 0; i < circles.size(); i++)
 	{
-		for (int y = 0; y < src.cols; y++) 
-		{
-			if ((src.at<Vec3b>(x, y)[0] >= 240) &&
-				(src.at<Vec3b>(x, y)[1] >= 240) &&
-				(src.at<Vec3b>(x, y)[2] >= 240)) 
-			{
-				src.at<Vec3b>(x, y)[0] = 0;
-				src.at<Vec3b>(x, y)[1] = 0;
-				src.at<Vec3b>(x, y)[2] = 0;
-			}
-		}
+		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+		int radius = cvRound(circles[i][2]);
+
+		circle(src, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+		circle(src, center, radius, Scalar(0, 0, 255), 3, 8, 0);
 	}
 
-	imshow("Black Background Image", src);
-
-	Mat kernel = (Mat_<float>(3, 3) <<
-		1, 1, 1,
-		1, -12, 1,
-		1, 1, 1);
-
-	Mat imgLaplacian;
-	Mat sharp = src;
-	filter2D(sharp, imgLaplacian, CV_32F, kernel);
-	src.convertTo(sharp, CV_32F);
-
-	Mat imgResult = sharp - imgLaplacian;
-	imgResult.convertTo(imgResult, CV_8UC3);
-	imgLaplacian.convertTo(imgLaplacian, CV_8UC3);
-
-	vector<vector<Point> > contours;
-	Mat imgLaplacianGray;
-	cvtColor(imgLaplacian, imgLaplacianGray, CV_BGR2GRAY);
-	findContours(imgLaplacianGray, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-	for (size_t i = 0; i < contours.size(); i++)
-	{
-		drawContours(
-			imgResult, 
-			contours, 
-			static_cast<int>(i), 
-			Scalar::all((static_cast<int>(i) + 100) % 255), 
-			-1);
-	}
-
-	imshow("Borders", imgResult);
+	imshow("Result", src);
 
 	waitKey(0);
 	return 0;
