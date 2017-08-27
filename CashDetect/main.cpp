c75a87fab5e29a9c78174b361accdace4c9dbaa9
NataliaSlobodianiuk <slo0nata@gmail.com>
2017-08-27 16:40:12 +0300

    getRGBDifference deleted, getSaturationAvg added, getCoinValue updated (coins recognition is now based on saturation)

--- CashDetect/main.cpp
+++ CashDetect/main.cpp
@@ -12,7 +12,7 @@ using namespace cv;
 using namespace std;
 
 int getCoinValue(Mat& img, Point center, double radius, double min_radius);
-double getBGRDifference(Mat& img, Point center);
+double getSaturationAvg(Mat& img, Point center);
 vector<Vec3f> getCircles(Mat& src_1C, double min_radius);
 
 void sortVerteces(vector<Point2f>& verteces);
@@ -21,28 +21,9 @@ void fourVertecesTransform(Mat& src, vector<Point2f> verteces);
 int getCoinsSum(Mat& src);
 Mat getA4(Mat& src);
 
-int kernel3x3[] = {
-	1, 1, 1,
-	1, 1, 1,
-	1, 1, 1
-};
-
-int kernel5x5[] = {
-	2, 0, 2, 0, 2,
-	0, 2, 0, 2, 0,
-	2, 0, 2, 0, 2,
-	0, 2, 0, 2, 0,
-	2, 0, 2, 0, 2,
-};
-
-Mat kernel = Mat(5, 5, CV_8UC1, kernel5x5);
-
 int main(int argc, char **argv)
 {
 	Mat src;
-	Mat src_gray, src_gray_copy;
-	Mat mask;
-	vector<vector<Point>> contours;
 
 	for (int i = 1; i < argc; i++)
 	{
@@ -100,69 +81,65 @@ int getCoinValue(Mat& img, Point center, double radius, double min_radius)
 {
 	int value = -1;
 
-	double bgr_difference = getBGRDifference(img, center);
+	double s_avg = getSaturationAvg(img, center);
 
-	if (radius > min_radius && radius < min_radius * 1.1)
+	//saturation for 10, 25, 50 and 100 will be higher than 70
+	//saturation for 1, 2, 5 will be lower than 35
+	//if saturation is in range [35, 70] the coin is not verified
+	if (s_avg > 70)
 	{
-		if (bgr_difference > 10)
+		if (radius >= min_radius && radius < min_radius * 1.1)
 		{
 			value = 10;
 		}
-		else
+		else if (radius >= min_radius * 1.27 && radius <= min_radius * 1.4)
 		{
-			value = 1;
+			value = 25;
 		}
-	}
-	else if (radius < min_radius * 1.15 && bgr_difference < 5)
-	{
-		value = 2;
-	}
-	else if (radius > min_radius * 1.35 && 
-		radius < min_radius * 1.45 && 
-		bgr_difference > 10)
-	{
-		value = 25;
-	}
-	else if (radius > min_radius * 1.5 && radius < min_radius * 1.6)
-	{
-		if (bgr_difference > 10)
+		else if (radius >= min_radius * 1.43 && radius <= min_radius * 1.57)
 		{
 			value = 50;
 		}
-		else
+		else if (radius >= min_radius * 1.58 && radius <= min_radius * 1.75)
+		{
+			value = 100;
+		}
+	}
+	else if (s_avg < 35)
+	{
+		if (radius >= min_radius && radius < min_radius * 1.1)
+		{
+			value = 1;
+		}
+		else if (radius <= min_radius * 1.15)
+		{
+			value = 2;
+		}
+		else if (radius >= min_radius * 1.43 && radius <= min_radius * 1.57)
 		{
 			value = 5;
 		}
-	}
-	else if (radius > min_radius * 1.68 &&
-		radius < min_radius * 1.72 && 
-		bgr_difference > 10)
-	{
-		value = 100;
 	}
 
 	return value;
 }
 
-double getBGRDifference(Mat& img, Point center)
+double getSaturationAvg(Mat& img, Point center)
 {
-	double bgr_difference = 0;
+	Mat hsv;
+	cvtColor(img, hsv, CV_RGB2HSV);
 
-	for (int i = -3; i <= 3; i++)
+	double s_avg = 0;
+
+	for (int i = -5; i <= 5; i++)
 	{
-		Vec3b x_axis = img.at<Vec3b>(Point(center.x + i, center.y));
-		bgr_difference += max<uchar>(
-			max<uchar>(abs(x_axis[0] - x_axis[1]), abs(x_axis[0] - x_axis[2])),
-			abs(x_axis[1] - x_axis[2]));
-		Vec3b y_axis = img.at<Vec3b>(Point(center.x, center.y + i));
-		bgr_difference += max<uchar>(
-			max<uchar>(abs(y_axis[0] - y_axis[1]), abs(y_axis[0] - y_axis[2])),
-			abs(y_axis[1] - y_axis[2]));
+		s_avg += hsv.at<Vec3b>(center.y, center.x + i)[1];
+		s_avg += hsv.at<Vec3b>(center.y + i, center.x)[1];
 	}
 
-	bgr_difference /= 13;
+	s_avg /= 21;
 
-	return bgr_difference;
+	return s_avg;
 }
 
 vector<Vec3f> getCircles(Mat& src_1C, double min_radius)
@@ -176,11 +153,11 @@ vector<Vec3f> getCircles(Mat& src_1C, double min_radius)
 		circles,
 		HOUGH_GRADIENT,
 		1,
-		min_radius * 1.5,
+		min_radius,
 		30,
 		50,
 		min_radius,
-		min_radius * 2);
+		min_radius * 1.75);
 
 	return circles;
 }
@@ -192,36 +169,43 @@ int getCoinsSum(Mat& src)
 	namedWindow("Gray", CV_WINDOW_FREERATIO);
 	imshow("Gray", src_gray);
 
-	double min_radius = (double)max<int>(src_gray.cols, src_gray.rows) / 40;
+	double min_radius = (double)max<int>(src_gray.cols, src_gray.rows) / 38;
 
 	vector<Vec3f> circles = getCircles(src_gray, min_radius);
 
-	cout << "Number of coins: " << circles.size() << ".\n";
+	Mat src_copy;
+	src.copyTo(src_copy);
 
 	int sum = 0;
+	int count = 0;
 	for (size_t i = 0; i < circles.size(); i++)
 	{
 		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-		int radius = cvRound(circles[i][2]);
+		double radius = circles[i][2];
 
 		int value = getCoinValue(src, center, radius, min_radius);
 		if (value != -1)
 		{
 			sum += value;
+			count++;
 		}
 
-		circle(src, center, 1, Scalar(0, 255, 0), 3, 8, 0);
-		circle(src, center, radius, Scalar(255, 0, 0), 5, 8, 0);
+		circle(src_copy, center, 1, Scalar(0, 255, 0), 3, 8, 0);
+		circle(src_copy, center, radius, Scalar(255, 0, 0), 5, 8, 0);
 
 		putText(
-			src,
-			value == -1 ? "unknown coin" : to_string(value),
+			src_copy,
+			value == -1 ? "unknown" : to_string(value),
 			Point(center.x - radius, center.y - radius),
 			FONT_HERSHEY_SCRIPT_COMPLEX,
-			3,
+			1.5,
 			Scalar(0, 0, 255),
 			3);
 	}
+	src = src_copy;
+
+	cout << "Number of recognized coins: " << circles.size() << ".\n";
+
 	return sum;
 }
 
