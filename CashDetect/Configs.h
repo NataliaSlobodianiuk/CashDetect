888f90dadb85df53ea756e5239c2b4604e8a7547
NataliaSlobodianiuk <slo0nata@gmail.com>
2017-08-27 22:43:36 +0300

    listCurrencyDB_u.txt moved to data folder, path in Config.h changed

--- CashDetect/Configs.h
+++ CashDetect/Configs.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Defines for project
-#define TEMPATES_FILENAME "listCurrencyDB_u.txt"
+#define TEMPATES_FILENAME "..\\data\\listCurrencyDB_u.txt"
 #define TEMPLATES_DIR "..\\data\\currencyDB_lowResolution_m\\"
 #define EXTENSION ".jpg"
 #define TEST_DB "..\\data\\examples\\"
